ac3b0582c6489f54119ae5f7b0afa481388b6866
gozdece <gozdecengiiz98@gmail.com>
2022-01-05 16:58:50 +0300

    first commit

---
+++ 3.Ders.Console/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("İsminizi girin:");
+string name = Console.ReadLine();
+Console.WriteLine("Soyisminizi girin:");
+string surname=Console.ReadLine();
+System.Console.WriteLine("Merhaba"+name+" "+surname);
