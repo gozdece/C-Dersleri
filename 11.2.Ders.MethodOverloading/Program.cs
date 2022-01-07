39bb9a9982449efe64bb803e09277b0f1d9dc282
gozdece <gozdecengiiz98@gmail.com>
2022-01-07 16:41:57 +0300

    C# Patikası 11.Ders commiti.

---
+++ 11.2.Ders.MethodOverloading/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string sayi ="999";
+
+bool sonuc = int.TryParse(sayi,out int outSayi);
+
+if(sonuc)
+    System.Console.WriteLine("String olan sayıyı çevirdim:"+outSayi);
+else    
+    System.Console.WriteLine("Başarısız.");
+
+
+Methods methods = new Methods();
+methods.Topla(3,5,out int toplam);
+System.Console.WriteLine("Topla methodu ile yaptığımız işlemin sonucu: {0}",toplam);
+
+//Methods Overloading
+System.Console.WriteLine("ekranayazdır metodu int değer alıyor:");
+methods.EkranaYazdir(5);
+System.Console.WriteLine("ekranayazdır metodu string değer alıyor:");
+methods.EkranaYazdir("hey");
+class Methods{
+    public void Topla(int a,int b,out int outToplam)
+    {
+        outToplam = a+b;
+    }
+    
+    public void EkranaYazdir(string veri)
+    {
+        System.Console.WriteLine(veri);
+    }
+    public void EkranaYazdir(int veri)
+    {
+        System.Console.WriteLine(veri);
+    }
+}
