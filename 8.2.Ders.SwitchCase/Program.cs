3d7b28d6ab12bf766b1235ee580766e7b3cd86d1
gozdece <gozdecengiiz98@gmail.com>
2022-01-07 10:30:50 +0300

    C# Patikası 8.2.Ders commiti.

---
+++ 8.2.Ders.SwitchCase/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int month = DateTime.Now.Month;
+
+switch (month)
+{
+    case 1:
+        System.Console.WriteLine("Ocak ayındasınız.");
+        break;
+    case 2: 
+        System.Console.WriteLine("Şubat ayındasınız");
+        break;
+    default:
+        System.Console.WriteLine("Burası başka bir ay");
+        break;
+}
+
+switch (month)
+{   
+    case 12:
+    case 1:
+    case 2:
+        System.Console.WriteLine("Kış ayları");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        System.Console.WriteLine("Yaz ayları");
+        break;
+    default:
+        break;
+}
