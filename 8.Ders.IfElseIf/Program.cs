27085b198f5eef630fb4d755167dd34bd8e7d7de
gozdece <gozdecengiiz98@gmail.com>
2022-01-07 10:13:13 +0300

    C# Patikası 8.Ders commiti.

---
+++ 8.Ders.IfElseIf/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int hour = DateTime.Now.Hour;
+
+if(hour>=6 && hour<=11)
+    System.Console.WriteLine("Gunaydin");
+else if(hour>17)
+    System.Console.WriteLine("İyi öğlenler");
+else
+    System.Console.WriteLine("İyi aksamlar");
+
+string sonuc = hour>=6 && hour<=11 ? "Günaydın" : hour>17 ? "İyi öğlrenler" : "İyi Aksamlar";
+System.Console.WriteLine(sonuc);
