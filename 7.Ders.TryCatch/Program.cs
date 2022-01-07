18f21e2614fe6e95b0a29c4cd43fe38eb7ef0cdb
gozdece <gozdecengiiz98@gmail.com>
2022-01-07 09:59:09 +0300

    C# Patikası 7.Ders commiti.

---
+++ 7.Ders.TryCatch/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+try
+{
+     System.Console.WriteLine("Bir değer girin:");
+     int rakam = Convert.ToInt32(Console.ReadLine());
+     System.Console.WriteLine("Girdiğiniz değer:" + rakam);
+}
+catch (System.Exception ex)
+{
+    System.Console.WriteLine("Hata:"+ex.Message);
+}finally{
+    System.Console.WriteLine("Finally");
+}
+
+try
+{
+     int rakam = int.Parse(null);
+}
+catch (System.ArgumentException ex)
+{
+    System.Console.WriteLine("Boş bir değer girdiniz");
+    System.Console.WriteLine("Hata:"+ex.Message);
+}
+*/
+try
+{   
+    System.Console.WriteLine("Bir sayı girin:");
+    int rakam = Convert.ToInt32(Console.ReadLine());
+}
+catch (System.OverflowException ex)
+{
+    System.Console.WriteLine("Geçersiz aralıkta");
+    System.Console.WriteLine("Hata:"+ex.Message);
+}
