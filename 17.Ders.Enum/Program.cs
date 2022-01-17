74ae1f97fe5ce6f355e56855ac2c910b1476a495
gozdecengiz <gozdecengiiz98@gmail.com>
2022-01-17 10:29:23 +0300

    C# Patikası 17.Ders commiti.

---
+++ 17.Ders.Enum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _17.Ders.Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Gunler.Cumartesi);
+            System.Console.WriteLine((int)Gunler.Sali);
+            
+            int sicaklik =28;
+            if (sicaklik<(int)HavaDurumu.Soguk)
+            {
+                System.Console.WriteLine("hava soguk altinda");
+                
+            }else if(sicaklik<(int)HavaDurumu.Normal && sicaklik>(int)HavaDurumu.Soguk)
+            {
+                System.Console.WriteLine("hava soguk ve normal arasinda");
+
+            }else if(sicaklik<(int)HavaDurumu.Sicak && sicaklik>(int)HavaDurumu.Normal)
+            {
+                System.Console.WriteLine("hava sicak ve normal arasinda");
+            }else
+            {
+                System.Console.WriteLine("hava sicak");
+            }
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi=1,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal =15,
+        Sicak =30
+    }
+}
